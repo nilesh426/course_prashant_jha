6b96994bbbeeb645a480c797966d69cf203fd9ea
nilesh426 <nileshasawant426@gmail.com>
2026-04-02 16:51:15 +0530

    DAY1 complete

---
+++ DAY1/conditional7.py
@@ -0,0 +1,9 @@
+Amount = int(input("Enter the amount for withdrawal: "))
+
+print("100 notes=", Amount//100)
+print("50 notes=", (Amount%100)//50)
+print("20 notes=", ((Amount%100)%50)//20)   
+print("10 notes=", (((Amount%100)%50)%20)//10)
+print("5 notes=", ((((Amount%100)%50)%20)%10)//5)
+print("2 notes=", (((Amount%100)%50)%20)%10%5//2)
+print("1 notes=", (((Amount%100)%50)%20)%10%5%2//1)
