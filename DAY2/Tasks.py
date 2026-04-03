eccd2f3f50748b59c0ed6c6f44daf4d2a50ae5cb
nilesh426 <nileshasawant426@gmail.com>
2026-04-03 16:05:28 +0530

    change

--- DAY2/Tasks.py
+++ DAY2/Tasks.py
@@ -10,3 +10,72 @@
 #         A.append(i)
 # print(A) #[4, 2, 5, 1, 0, 0]
 
+# WAP to remove duplicates from the list
+# L=[1,2,2,3,3,4,4,5]
+# a=[]
+# for i in L:
+#     if i not in a:
+#         a.append(i)
+# print(L)
+# print(a) #[1, 2, 3, 4, 5]
+
+
+#WAP to find the common elements in three lists
+#  a=[1,2,3]
+# b=[2,3,4]
+# c=[3,4,5]
+# for i in (a):
+#     if i in b and i in c:
+#         print(i)
+
+#WAP to find distance between ajacent elements and return the sum of those distances
+# n = int(input("Enter size of array:"))
+# arr=[]
+# for i in range(n):
+#     val = int(input("Enter element:"))
+#     arr.append(val)
+# sum=0
+# print(arr)
+# for i in range(n):
+#     if i+1 in range(n):
+#         sum+= abs(arr[i]-arr[i+1])
+# print(sum)
+
+# for i in range(1,5):
+#      if i==3:
+#         break
+#      print(i)
+
+# for i in range(1,5):
+#      if i==3:
+#         continue
+#      print(i)
+
+# for i in range(1,5):
+#      if i==3:
+#         break
+#      print(i)
+
+
+#zip() we can take multiple range in same loop
+# for i,j in zip(range(1,6),range(5,0,-1)):
+#      if i==3:
+#         continue
+#      print(i,"  ",j)
+
+# for i,j in zip(range(1,7),range(6,0,-1)):
+#         if i==3 and j==3:
+#              continue
+#         print(i,"  ",j)
+
+
+# a = "prashant*is*a*good*programmer"
+# newstr=""
+# for i in a:
+#     if i!="*":
+#         newstr = newstr + i
+#     else:
+#         newstr = i + newstr
+# print(newstr) #***prashantisagoodprogrammer
+
+
