7452512ef05ef6a715e448a4b7fc578e4966974b
nilesh426 <nileshasawant426@gmail.com>
2026-04-03 13:00:30 +0530

    change

--- DAY2/list1.py
+++ DAY2/list1.py
@@ -50,8 +50,46 @@ mylist = ["prashant","ankush","ashish","satyarth","77","yash","nilesh"]
 # list2 = [1,2,3,4,5]
 # print(list1+list2)
 
-list2 = [1,2,3,4,5]
-# del list2[1]
-# print(list2) #NameError: name 'list2' is not defined
-list2.clear()
-print(list2) #[]
+# list2 = [1,2,3,4,5]
+# # del list2[1]
+# # print(list2) #NameError: name 'list2' is not defined
+# list2.clear()
+# print(list2) #[]
+
+# name = "nilesh"
+# print(name)
+# myname = list(name)
+# print(myname) #['n', 'i', 'l', 'e', 's', 'h']
+
+# mylist = [40,50,60,67]
+# mylist.reverse()
+# print(mylist) #[67, 60, 50, 40]
+
+# list = [9,56,10,53,78,45]
+# list.sort()
+# print(list) #[9, 10, 45, 53, 56, 78] ascending oder
+
+# list.sort(reverse=True)
+# print(list) #[78, 56, 53, 45, 10, 9]  reverse=True-->descending order
+
+# #Alising
+# mylist = [44,55,66,77,88,99]
+# newlist = mylist
+# print(id(mylist))
+# print(id(newlist)) 
+# mylist[0]="append"
+# print(mylist) #[44, 55, 66, 77, 88, 99]
+# print(newlist) #[44, 55, 66, 77, 88,
+
+# arr = [[1,2,3,4],
+#        [4,5,6,7],
+#        [7,8,9,10],
+#        [11,12,13,14]]
+# for i in range(0,4):
+#     print(arr[i].pop())
+
+arr=[1,2,3,4,5,6]
+for i in range(1,6):
+    arr[i-1]=arr[i]
+for i in range(0,6):
+    print(arr[i],end=" ")
