4bc925d8f1c2bc2d8de3df859b4d72c44883e2f8
nilesh426 <nileshasawant426@gmail.com>
2026-04-06 21:30:29 +0530

    DAY 5 complete

--- DAY5/Polymorphism.py
+++ DAY5/Polymorphism.py
@@ -88,24 +88,24 @@
 # obj.car_loan()
 
 #Using super() to access 
-class Rbi:
-    def home_loan(self):
-        print("Home loan interest rate is 7%")
-    def car_loan(self):
-        print("Car loan interest rate is 8%")
+# class Rbi:
+#     def home_loan(self):
+#         print("Home loan interest rate is 7%")
+#     def car_loan(self):
+#         print("Car loan interest rate is 8%")
         
         
-class Sbi(Rbi):
-    def home_loan(self):
-        print("Sbi Provide home loan at 6 interest rate")
-    def car_loan(self):
-        print("Car loan interest rate is 7")
-        super().home_loan() 
-        super().car_loan()  #calling method of parent class using super() function
+# class Sbi(Rbi):
+#     def home_loan(self):
+#         print("Sbi Provide home loan at 6 interest rate")
+#     def car_loan(self):
+#         print("Car loan interest rate is 7")
+#         super().home_loan() 
+#         super().car_loan()  #calling method of parent class using super() function
         
-obj = Sbi()   #creating object of child class
-obj.home_loan()   #calling method of child class using child
-obj.car_loan()    #calling method of child class using child
+# obj = Sbi()   #creating object of child class
+# obj.home_loan()   #calling method of child class using child
+# obj.car_loan()    #calling method of child class using child
 
 
 
