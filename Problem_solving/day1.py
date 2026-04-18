3b00f8e53af0c6d04afb9ca6e8ae04845ffff29c
nilesh426 <nileshasawant426@gmail.com>
2026-04-19 01:12:42 +0530

    problem solving

---
+++ Problem_solving/day1.py
@@ -0,0 +1,29 @@
+# Complete the 'camelcase' function below.
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+
+# def camelcase(s):
+#     # Write your code here
+#     count=1
+  
+#     for i in s:
+#         if i==i.upper():
+#             count+=1
+            
+#     return count
+
+# Anagram example
+# a="silent"
+# b="listen"
+# if len(a)==len(b):
+#     for i in a:
+#         if i not in b:
+#             print("Not anagram")
+#             break
+#     else:
+#         print("Anagram")
+# else:
+#     print("Not anagram")
+
+#
+
