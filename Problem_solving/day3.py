79dcbca9a6394113e46a1b06a9ccaa0f510f569a
nilesh426 <nileshasawant426@gmail.com>
2026-08-05 22:56:34 +0530

    changes

--- Problem_solving/day3.py
+++ Problem_solving/day3.py
@@ -2,11 +2,14 @@
 # print((sum(bill)-bill[1])/2)
 
 
-unsorted=['1','200','150','3']
-for i in range(len(unsorted)):
-    for j in range(len(unsorted)):
-        if unsorted[i] > unsorted[j]:
-            unsorted[i], unsorted[j] = unsorted[j], unsorted[i]
+# unsorted=['1','200','150','3']
+# un1=list(map(int,unsorted))
+# n=len(un1) 
+# for i in range(n):
+#     for j in range(i+1,n):
+#         if un1[i]>un1[j]:
+#             un1[i],un1[j] = un1[j],un1[i]
 
-print(unsorted)           
+# print(list(map(str,un1)))           
         
+
